6a815efcab79bf0a421ec05faf618020757cd9dd
rafalsr <rafael.ribeiro@uniriotec.br>
2016-05-29 16:01:44 -0300

    Importação Inicial Projeto
    Apenas inportando inicialmente o projeto para o git.

---
+++ src/Aluno.java
@@ -0,0 +1,52 @@
+
+import java.util.List;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Rafael
+ */
+public class Aluno {
+    
+    private String nome;
+    private String curso;
+    
+    private List<Artigo> artigos;
+
+    public Aluno(String nome, String curso, List<Artigo> artigos) {
+        this.nome = nome;
+        this.curso = curso;
+        this.artigos = artigos;
+    }
+    
+    
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public List<Artigo> getArtigos() {
+        return artigos;
+    }
+
+    public void setArtigos(List<Artigo> artigos) {
+        this.artigos = artigos;
+    }
+}
---
+++ src/ProgramaPosGraduacao.java
@@ -0,0 +1,19 @@
+
+import java.util.Date;
+import java.util.List;
+
+
+
+/**
+ *  Classe que detém informações de Programa de Gradução.
+ * @author Rafael
+ */
+public class ProgramaPosGraduacao {
+    
+    List<Professor> professores;
+    List<Artigo> artigos;
+    List<Pesquisa> pesquisas;
+    Date ultimaAval;
+    int classificacao;
+    
+}
